c5a59c5cf2a1f74939489e8a3f9adb40fbc82993
Dominik79 <dominik.jasiok@gmail.com>
2021-06-10 15:33:31 +0200

    Test one

--- pages/models.py
+++ pages/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+
+
 # Create your models here.
 class Oddzialy(models.Model):
     nazwa = models.TextField(max_length=50)
